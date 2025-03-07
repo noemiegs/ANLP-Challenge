343d3419f3f40213bfaf6106e8d05e4d406aeeeb
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2025-03-07 16:47:23 +0000

    refactor

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ Comment run le code:
 pip install requirements
 
 Liste des fichiers:
-model
+
+eda: exploration
+
+model.py: interface python pour run un training et une inférence sur un modèle et des hyperparamètres au choix
+model_comparison.py: grid_search / test différents modèles et différentes config 
+
+model_intfloat.ipynb: notebook pour train et tester notre modèle final
+model_distilbert.ipynb: test effectués avec un autre modèle
+run_test_to_submit.ipynb: permet de générer le fichier de soumission kaggle 
 
 
 La compétition Kaggle est disponible sur le lien suivant: https://www.kaggle.com/competitions/nlp-cs-2025/leaderboard 
--- model_comparison.py
+++ model_comparison.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-from model_intfloat import NLPTrainer, split_data, augment_data, remove_urls, cross_validate
+from model import NLPTrainer, split_data, augment_data, remove_urls, cross_validate
 from transformers import set_seed
 
 # Paramètres globaux
@@ -47,11 +47,13 @@ def run_model_comparison():
         trainer = NLPTrainer(dataset)
         trainer.train()
         metrics = trainer.evaluate()
-        results.append({
-            "Model": model_name,
-            "Accuracy": metrics["accuracy"],
-            "F1": metrics["f1"]
-        })
+        print(f"📊 Résultats: {metrics}")
+        print(metrics.keys())
+        # results.append({
+        #     "Model": model_name,
+        #     "Accuracy": metrics["accuracy"],
+        #     "F1": metrics["f1"]
+        # })
 
     pd.DataFrame(results).to_csv(RESULTS_FILE_MODEL, index=False)
     print(f"✅ Résultats sauvegardés dans {RESULTS_FILE_MODEL}")
@@ -85,22 +87,24 @@ def run_config_tests():
         if merged_config["CROSS_VALIDATION"]:
             # Si CROSS_VALIDATION est True, utiliser la méthode de CV
             metrics = cross_validate(data_aug, n_splits=merged_config["CV_SPLITS"], stratify=merged_config["STRATIFY"])
-            results.append({
-                "Configuration": merged_config["name"],
-                "Accuracy": metrics["eval_accuracy"],
-                "F1": metrics["eval_f1"]
-            })
+            # results.append({
+            #     "Config": merged_config["name"],
+            #     "Accuracy": metrics["accuracy"],
+            #     "F1": metrics["f1"]
+            # })
         else:
             # Sinon, entraîner et évaluer normalement
             dataset = split_data(data_aug, use_stratify=merged_config["STRATIFY"])
             trainer = NLPTrainer(dataset)
             trainer.train()
             metrics = trainer.evaluate()
-            results.append({
-                "Configuration": merged_config["name"],
-                "Accuracy": metrics["eval_accuracy"],
-                "F1": metrics["eval_f1"]
-            })
+            print(f"📊 Résultats: {metrics}")
+            print(metrics.keys())
+            # results.append({
+            #     "Config": merged_config["name"],
+            #     "Accuracy": metrics["accuracy"],
+            #     "F1": metrics["f1"]
+            # })
 
     pd.DataFrame(results).to_csv(RESULTS_FILE_CONFIG, mode='a', header=False, index=False)
     print(f"✅ Résultats des configs sauvegardés dans {RESULTS_FILE_CONFIG}")
